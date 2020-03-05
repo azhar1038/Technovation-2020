4e6fe21e273a9327cdc340791b01a45accdb9066
mdazharuddin1011999@gmail <mdazharuddin1011999@gmail.com>
2020-03-05 14:57:40 +0530

    Final Release to public

--- lib/src/ui/about.dart
+++ lib/src/ui/about.dart
@@ -181,7 +181,7 @@ class _AboutState extends State<About> {
                     ),
                     SizedBox(height: 16),
                     Text(
-                      'Version: 1.8.9',
+                      'Version: 2.0.0',
                       style: my,
                     ),
                     SizedBox(height: 16),
